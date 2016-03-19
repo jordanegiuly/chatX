f4e2f70ff913a9b9853d27229585f2b0c595d9ef
Jordane Giuly <jordane.giuly@gmail.com>
2016-03-19 09:19:02 +0100

    add public dir and index.html file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+    res.sendFile('/index.html');
 });
 
 app.listen(app.get('port'), function(){
